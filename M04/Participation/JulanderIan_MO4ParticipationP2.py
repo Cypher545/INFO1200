ace76fc6eba65e0eefadfa7244088fd84d191a16
Ian Julander <11073864@uvu.edu>
2026-06-05 21:40:20 -0600

    finished P2

--- M04/Participation/JulanderIan_MO4ParticipationP2.py
+++ M04/Participation/JulanderIan_MO4ParticipationP2.py
@@ -4,13 +4,49 @@
 print("Welcome to Ian Julander's Future Value Calculator")
 print()
 
+# create variable for loop
 choice = "y"
+# start loop
 while choice.lower() == "y":
 
-    # get input from the user
-    monthly_investment = float(input("Enter monthly investment:\t"))
-    yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
-    years = int(input("Enter number of years:\t\t"))
+    #create variable for loop
+    is_valid = False
+    while is_valid == False:
+
+        # get input from user
+        monthly_investment = float(input("Enter monthly investment:\t"))
+        
+        # validate monthly investment
+        if monthly_investment > 0 and monthly_investment <= 1000:
+            is_valid = True
+        else:
+            print("Entry must be greater than 0 and less than or equal to 1000. Please try again.")
+        
+    # reset variable for loop    
+    is_valid = False
+    while is_valid == False:
+
+        # get input from user
+        yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+        
+        # validate yearly interest rate
+        if yearly_interest_rate > 0 and yearly_interest_rate <= 15:
+            is_valid = True
+        else:
+            print("Entry must be greater than 0 and less than or equal to 15. Please try again.")
+
+    # reset variable for loop
+    is_valid = False
+    while is_valid == False:
+
+        # get input from user
+        years = int(input("Enter number of years:\t\t"))
+
+        # validate years
+        if years > 0 and years <= 50:
+            is_valid = True
+        else:
+            print("Entry must be greater than 0 and less than or equal to 30. Please try again.")
 
     # convert yearly values to monthly values
     monthly_interest_rate = yearly_interest_rate / 12 / 100
